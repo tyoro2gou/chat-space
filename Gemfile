9c4fcae84bfcd3a3826390ab118b8c4a9b871c9a
tyoro2gou <asd4asd74@yahoo.co.jp>
2020-01-08 14:56:58 +0900

    update3

--- Gemfile
+++ Gemfile
@@ -55,3 +55,4 @@ gem 'haml-rails'
 gem 'font-awesome-sass'
 gem "font-awesome-rails"
 gem 'devise'
+gem 'pry-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'messages/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "messages#index"
-  resources :users, only: [:edit, :update]
+  root 'groups#index'
+  resources :users,  only: [:edit, :update]
+  resources :groups, only: [:index,:new, :create, :edit, :update]
 end
