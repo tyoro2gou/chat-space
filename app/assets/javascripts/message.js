e429dcf05ec6f92bc0c79b6147400844746df1f0
tyoro2gou <asd4asd74@yahoo.co.jp>
2020-01-15 16:46:55 +0900

    update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,7 @@
 $(function(){
-  function buildHTML(message){
-    if (message.image) {
+  
+  var buildHTML = function(message) {
+    if (message.content && message.image) {
       var html =
         `<div class="content" data-message-id=${message.id}>
           <div class="content__box">
@@ -18,8 +19,7 @@ $(function(){
             <img class="content__message_box--image" src=${message.image}>
           </div>
         </div>`
-      return html;
-    } else {
+    } else if (message.content){
       var html =
       `<div class="content" data-message-id=${message.id}>
         <div class="content__box">
@@ -36,10 +36,26 @@ $(function(){
           </p>
         </div>
       </div>`
-      return html;
+    } else if (message.image) {
+      var html =
+        `<div class="content" data-message-id=${message.id}>
+          <div class="content__box">
+            <div class="content__box--user-name">
+            ${message.user_name}
+            </div>
+            <div class="time">
+            ${message.created_at}
+            </div>
+          </div>
+          <div class="content__message_box">
+            <img class="content__message_box--image" src=${message.image}>
+          </div>
+        </div>`
     };
-  }
-
+      return html;
+  };
+  
+  // hidouki
   $('#new_message').on('submit',function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -65,4 +81,36 @@ $(function(){
       $('.form__submit').prop('disabled', false);
     });
   })
+
+
+  // zidoukousin
+  var reloadMessages = function() {
+    last_message_id = $('.content:last').data("message-id");
+    console.log(last_message_id)
+    $.ajax({
+      url: "api/messages",
+      type: 'GET',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages){
+      
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.chat-main__message_list').append(insertHTML);
+        $('.chat-main__message_list').animate({ scrollTop: $('.chat-main__message_list')[0].scrollHeight});
+        $("#new_message")[0].reset();
+        $(".form__submit").prop("disabled", false);
+      }
+    })
+    .fail(function(){
+      console.log('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)){
+    setInterval(reloadMessages, 5000);
+  }
 });
