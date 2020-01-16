124564681181188682d76038044eaa01dadabcb7
tyoro2gou <asd4asd74@yahoo.co.jp>
2020-01-16 19:34:43 +0900

    updated_production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '18.176.200.157/', user: 'ec2-user', roles: %w{app db web}
+server '18.176.200.157', user: 'ec2-user', roles: %w{app db web}
 
 # server-based syntax
 # ======================
